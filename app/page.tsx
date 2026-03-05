0f98c9f73025cf9c92ce340ab5ed49b06590237e
hosek.jakub <hosek.jakub@ssakhk.cz>
2026-03-05 21:51:14 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,82 @@
+'use client';
 import Image from "next/image";
+import Link from "next/link";
+import BlurText from "./components/BlurText";
+import AnimatedContent from './components/AnimatedContent'
+
+const handleAnimationComplete = () => {
+  console.log('Animation completed!');
+};
+
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+      <main className="relative min-h-screen font-[inter]">
+        <section className="relative z-10 flex flex-col items-center justify-center min-h-screen px-6 text-center bg-gradient-to-b from-[#064e3b]/40 via-[#78350f]/30 to-black overflow-hidden">
+          <BlurText
+              text="NENECHTE LUKÁŠE TRPĚT HLADEM!"
+              delay={180}
+              animateBy="words"
+              direction="bottom"
+              onAnimationComplete={handleAnimationComplete}
+              className="text-5xl md:text-7xl lg:text-7xl font-black text-white leading-[1.3]"
+          />
+
+          <BlurText
+              text="Každá bageta se počítá. Podpořte Lukáše v jeho nekončícím boji s prázdným žaludkem a pomozte mu získat jídlo ještě dnes!"
+              delay={50}
+              animateBy="words"
+              direction="bottom"
+              onAnimationComplete={handleAnimationComplete}
+              className="flex mt-5 text-lg md:text-2xl text-amber-50/80 max-w-4xl leading-relaxed justify-center"
+          />
+
+          <div className="mt-20 flex flex-col sm:flex-row items-center justify-center gap-5 w-full font-bold">
+            <AnimatedContent
+                distance={100}
+                direction="vertical"
+                reverse={false}
+                duration={0.8}
+                ease="power3.out"
+                initialOpacity={0}
+                animateOpacity
+                scale={1}
+                threshold={0.1}
+                delay={1}
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              <Link href="#" className="w-full sm:w-auto">
+                <button className="bg-white p-4.5 rounded-md text-[#30261C] text-xl">
+                  Přispět na bagetu
+                </button>
+              </Link>
+            </AnimatedContent>
+
+            <AnimatedContent
+                distance={100}
+                direction="vertical"
+                reverse={false}
+                duration={0.8}
+                ease="power3.out"
+                initialOpacity={0}
+                animateOpacity
+                scale={1}
+                threshold={0.1}
+                delay={1}
             >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+              <Link href="#" className="w-full sm:w-auto">
+                <button className=" p-4.5 rounded-md text-white text-xl">
+                  Zjistit více
+                </button>
+              </Link>
+            </AnimatedContent>
+          </div>
+        </section>
+
+        <section className={"min-h-screen font-[inter] items-center bg-black"}>
+          <h1 className={"flex text-5xl md:text-7xl lg:text-7xl justify-center items-center font-black text-white leading-[1.3]"}>Kdo jsme?</h1>
+        </section>
       </main>
-    </div>
+
+
   );
 }
