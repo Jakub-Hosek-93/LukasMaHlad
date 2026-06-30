16ff972633305a03d8fdaef829ec0457c3dda64c
hosek.jakub <hosek.jakub@ssakhk.cz>
2026-06-30 16:50:18 +0200

    fix: Image loading

--- app/page.tsx
+++ app/page.tsx
@@ -116,7 +116,7 @@ export default function Home() {
                 </div>
 
                 <div>
-                    <img src={"/bageta.png"} alt={"Bageta s kuřecími řízečky"} width={600} height={400} className={"mx-auto mt-20 rounded-lg shadow-lg"} />
+                    <img src={"/Bageta.png"} alt={"Bageta s kuřecími řízečky"} width={600} height={400} className={"mx-auto mt-20 rounded-lg shadow-lg"} />
                     <p className="text-gray-500 text-sm mt-2 flex justify-center pb-10">Ilustrační fotografie bagety ze 100leté</p>
                 </div>
             </section>
